921e8492e2406a0e7a57d225ee1043fbb705a60c
Alex Hapgood <ach1@vt.edu>
2023-07-15 19:49:35 -0400

    Initial setup + NEXT.JS Framework

---
+++ pages/index.tsx
@@ -0,0 +1,171 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {Avatar, IconButton, MenuItem} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const cards = [1, 2, 3, 4, 5, 6, 7];
+
+const theme = createTheme();
+
+export default function Album() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="relative">
+                <Toolbar>
+                    <Typography variant="h6" color="primary" component="div">
+                        ACH
+                    </Typography>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        id="menu-button"
+                        onClick={handleClick}
+                        sx={{ marginLeft: "auto" }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <MenuItem><Link href={"/"}>About Me</Link></MenuItem>
+                    <MenuItem><Link href={"/portfolio/acidrt"}>ACIDRT</Link></MenuItem>
+                    <MenuItem><Link href={"/test"}>TEST</Link></MenuItem>
+                </Menu>
+
+
+                </Toolbar>
+            </AppBar>
+            </Box>
+            <main>
+                {/* Hero unit */}
+                <Box
+                    sx={{
+                        pt: 8,
+                        pb: 6,
+                    }}
+                >
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
+                        >
+                            {/* eslint-disable-next-line react/no-unescaped-entities */}
+                            Hey, I'm Alex
+                        </Typography>
+                        <Avatar alt="Alex Hapgood" src="/headshot.jpeg" align="center" sx={{ margin: 'auto', width: 128, height: 128 }}/>
+                        <Typography variant="h7" align="center" color="text.secondary" paragraph sx={{marginTop: '20px'}}>
+                            JavaScript Developer | Full-Stack Engineer <br/>
+                            Developer for Sigora Solar <br/>
+                            <b><em>Open to new positions!</em></b><br/>
+                            Interested in new JS technologies, from front-end to server-side
+                        </Typography>
+                        <Stack
+                            sx={{ pt: 4 }}
+                            direction="row"
+                            spacing={2}
+                            justifyContent="center"
+                        >
+                            <Button variant="contained">Main call to action</Button>
+                            <Button variant="outlined">Secondary action</Button>
+                        </Stack>
+                    </Container>
+                </Box>
+                <Container sx={{ py: 8 }} maxWidth="md">
+                    {/* End hero unit */}
+                    <Grid container spacing={4}>
+                        {cards.map((card) => (
+                            <Grid item key={card} xs={12} sm={6} md={4}>
+                                <Card
+                                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                >
+                                    <CardMedia
+                                        component="img"
+                                        sx={{
+                                            // 16:9
+                                            pt: '56.25%',
+                                        }}
+                                        image="https://source.unsplash.com/random"
+                                        alt="random"
+                                    />
+                                    <CardContent sx={{ flexGrow: 1 }}>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Heading
+                                        </Typography>
+                                        <Typography>
+                                            This is a media card. You can use this section to describe the
+                                            content.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small">View</Button>
+                                        <Button size="small">Edit</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+            {/* Footer */}
+            <Box sx={{ p: 6 }} component="footer">
+                <Typography variant="h6" align="center" gutterBottom>
+                    Footer
+                </Typography>
+                <Typography
+                    variant="subtitle1"
+                    align="center"
+                    color="text.secondary"
+                    component="p"
+                >
+                    Something here to give the footer a purpose!
+                </Typography>
+                <Copyright />
+            </Box>
+            {/* End footer */}
+        </>
+    );
+}
---
+++ pages/portfolio/acidrt.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function ACIDRT () {
+    return(<>
+        <h1>ACIDRT Portfolio Page</h1>
+        <h2>
+            <Link href="/">Back to home</Link>
+        </h2>
+    </>)
+}
